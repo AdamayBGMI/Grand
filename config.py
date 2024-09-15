211c63f5059837706e183ed98e6cb778fdf0ee0b
GrandFox18 <data828aop@gmail.com>
2024-09-15 23:34:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7379814138:AAFjaHstvbHUm2hhbjx-VTrH7A-PtopCj-U")
+BOT_TOKEN=("7283028754:AAGk-ZZe7Q_aQUkwCk9GU_cu_aIk984SdAs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@NINJASG")
+OWNER_USERNAME=("@Samahi18_09")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("894109871")
+ADMIN_IDS=("5870168842")
 
 
 
