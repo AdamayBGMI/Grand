1660959a39b173353a7963506a9bd263d0210a92
AdamayBGMI <amjhssre.01292@gmail.com>
2024-09-16 16:03:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7283028754:AAGk-ZZe7Q_aQUkwCk9GU_cu_aIk984SdAs")
+BOT_TOKEN=("7444230418:AAFsGQRQXGfqpcYcFcRHyBeAVXTk_FXNKUI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Samahi18_09")
+OWNER_USERNAME=("@ADAMAY_JAWALIA")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5870168842")
+ADMIN_IDS=("5941754045")
 
 
 
